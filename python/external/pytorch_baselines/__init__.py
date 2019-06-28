e9df08e837ad48e9f376a3ef6d6ffd9a93521f21
Dhruv Mauria Saxena <dhruvmsaxena@gmail.com>
2019-06-27 17:36:53 -0700

    fixed baseline from submodule to folder

---
+++ python/external/pytorch_baselines/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
+
+from a2c_ppo_acktr import algo, utils
+from a2c_ppo_acktr.algo import gail
+from a2c_ppo_acktr.arguments import get_args
+from a2c_ppo_acktr.envs import VecPyTorch, make_vec_envs
+from a2c_ppo_acktr.model import Policy
+from a2c_ppo_acktr.storage import RolloutStorage
+from a2c_ppo_acktr.utils import get_render_func, get_vec_normalize
+from evaluation import evaluate
